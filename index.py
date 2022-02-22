1584f3701c50577ebae9611f0cca4de580a8112e
nori3636 <nori.k3636@gmail.com>
2022-02-22 12:36:22 +0900

    flask-loginの追加

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, redirect, url_for, render_template
-# from flask_login import login_user, logout_user, LoginManager, UserMixin, login_required, current_user
+from flask_login import login_user, logout_user, LoginManager, UserMixin, login_required, current_user
 
 # from functions.schedule_manager import schedule_manager
 # from functions.account_manager import account_manager
@@ -14,24 +14,24 @@ app = Flask(__name__)
 # app.secret_key = os.urandom(24)
 
 # # ログイン機能のセットアップ
-# login_manager = LoginManager()
-# login_manager.init_app(app)
-# login_manager.login_view = "login" # ログインしてない時に飛ばされる場所
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login" # ログインしてない時に飛ばされる場所
 
 # firebaseの設定を読み込む
 # cred = credentials.Certificate()
 # firebase_admin.initialize_app(cred)
 # db = firestore.client()
 
-# ユーザークラスを定義
-# class  User(UserMixin):
-#     def __init__(self,uid):
-#         self.id = uid
+#ユーザークラスを定義
+class  User(UserMixin):
+    def __init__(self,uid):
+        self.id = uid
 
 
-# @login_manager.user_loader
-# def user_loader(uid:str):
-#     return User(uid)
+@login_manager.user_loader
+def user_loader(uid:str):
+    return User(uid)
 
 
 @app.route('/')
@@ -68,29 +68,29 @@ def signup():
     #     return str(uid)
 
 
-# @app.route('/calendar')
-# @login_required
-# def calendar():
-#     return f'''
-#     <h1>{current_user.id}さんのカレンダー<h1>
-#     <a href='regist'>登録</a>
-#     <a href='logout'>logout</a>
-#     '''
+@app.route('/calendar')
+@login_required
+def calendar():
+    return f'''
+    <h1>{current_user.id}さんのカレンダー<h1>
+    <a href='regist'>登録</a>
+    <a href='logout'>logout</a>
+    '''
 
 
-# @app.route('/regist')
-# @login_required
-# def regist():
-#     return f'''
-#     <h1>登録ページ<h1>
-#     <a href='calendar'>カレンダーに戻る</a>
-#     '''
+@app.route('/regist')
+@login_required
+def regist():
+    return f'''
+    <h1>登録ページ<h1>
+    <a href='calendar'>カレンダーに戻る</a>
+    '''
 
 
-# @app.route('/logout')
-# def logout():
-#     logout_user()
-#     return redirect(url_for("login"))
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
 
 
 # run the app.
