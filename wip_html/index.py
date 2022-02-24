bc001dde3c550709c6b4438b8418c3dd7f81930c
nori3636 <nori.k3636@gmail.com>
2022-02-24 17:10:44 +0900

    予定の追加(重要度を除くまで)

---
+++ wip_html/index.py
@@ -0,0 +1,115 @@
+from argparse import Namespace
+from flask import Flask, request, redirect, url_for, render_template
+from flask_login import login_user, logout_user, LoginManager, UserMixin, login_required, current_user
+import os
+
+app = Flask(__name__)
+
+app.secret_key = os.urandom(24)
+
+# ログイン機能のセットアップ
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login" # ログインしてない時に飛ばされる場所3
+users = {'dev@mail.com': {'password': 'secret'}}
+
+class  User(UserMixin):
+    pass
+
+
+@login_manager.user_loader
+def user_loader(email):
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+    return user
+
+
+@login_manager.request_loader
+def request_loader(req):
+    email = req.form.get('email')
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+
+    return user
+
+
+@app.route('/')
+def index():
+    namae='6210'
+    tuki='2'
+    hi='20'
+    youbi='金'
+    yotei={'namae':'6210','yoteimei':'正月','nissuu':'2','zyuuyoudo':'特急'}
+    yotei2={'namae':'6210','yoteimei':'クリスマス','nissuu':'3','zyuuyoudo':'特急'}
+    yotei3={'namae':'6210','yoteimei':'元旦','nissuu':'5','zyuuyoudo':'特急'}
+    yotei4={'namae':'6210','yoteimei':'七五三','nissuu':'10','zyuuyoudo':'快速'}
+    yotei5={'namae':'6210','yoteimei':'こどもの日','nissuu':'4','zyuuyoudo':'普通'}
+    return render_template('calender.html', yotei=yotei,namae=namae,tuki=tuki,hi=hi,youbi=youbi,yotei2=yotei2,yotei3=yotei3,yotei4=yotei4,yotei5=yotei5)
+    
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+    if request.method == 'GET':
+        return '''
+                <h1>節目カレンダー<h1>
+                <form action='login' method='POST'>
+                <input type='text' name='name' id='name' placeholder='name'/>
+                <input type='password' name='password' id='password' placeholder='password'/>
+                <input type='submit' name='submit'/>
+               </form>
+               '''
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return '''
+                <h1>節目カレンダー<h1>
+                <form action='login' method='POST'>
+                <input type='text' name='name' id='name' placeholder='name'/>
+                <input type='password' name='password' id='password' placeholder='password'/>
+                <input type='submit' name='submit'/>
+               </form>
+               '''
+
+    name = request.form['name']
+    if request.form['password'] == users[name]['password']:
+        user = User()
+        user.id = name
+        login_user(user)
+        return redirect(url_for("calendar"))
+
+    return 'Bad login'
+
+
+@app.route('/calendar')
+@login_required
+def calendar():
+    return f'''
+    <h1>{current_user.id}さんのカレンダー<h1>
+    <a href='regist'>登録</a>
+    <a href='logout'>logout</a>
+    '''
+
+@app.route('/regist')
+@login_required
+def regist():
+    return f'''
+    <h1>登録ページ<h1>
+    <a href='calendar'>カレンダーに戻る</a>
+    '''
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
+
+# run the app.
+if __name__ == "__main__":
+    app.debug = True
+    app.run(port=5000)
